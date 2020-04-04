4ffe56df747823f9bec32d1e38a222757924e9cd
rizalhamdana <ari.gusti12@gmail.com>
2020-04-04 19:23:28 +0700

    add catch block when there are some error response from citizen service

--- api-gateway/routers/citizens.js
+++ api-gateway/routers/citizens.js
@@ -10,20 +10,30 @@ router.get('/citizens', isAuth, (req, res) => {
     var fullPath = '/api/v1/citizens';
     api.get(fullPath).then(resp => {
         res.send(resp.data);
+    }).catch(error => {
+        res.status(error.response.status);
+        res.send(error);
     });
 });
 router.get('/citizens/:nik', isAuth, (req, res) => {
     var nik = req.params['nik'];
     var fullPath = '/api/v1/citizens/' + nik;
     api.get(fullPath).then(resp => {
+        console.log(resp)
         res.send(resp.data);
+    }).catch(error => {
+        res.status(error.response.status);
+        res.send(error);
     });
 });
 
 router.post('/citizens', isAuth, (req, res) => {
-    var fullPath = '/api/v1/citizens/'
+    var fullPath = '/api/v1/citizens'
     api.post(fullPath, req.body).then(resp => {
         res.send(resp);
+    }).catch(error => {
+        res.status(error.response.status);
+        res.send(error);
     });
 });
 
@@ -32,14 +42,20 @@ router.delete('/citizens/:nik', isAuth, (req, res) => {
     var fullPath = '/api/v1/citizens/' + nik;
     api.delete(fullPath).then(resp => {
         res.send(resp.data);
+    }).catch(error => {
+        res.status(error.response.status);
+        res.send(error);
     });
 });
 
-router.put('/citizens/:nik', isAuth, (req, res) => {
+router.put('/citizens/verif/:nik', isAuth, (req, res) => {
     var nik = req.params['nik'];
-    var fullPath = '/api/v1/citizens/' + nik;
+    var fullPath = '/api/v1/citizens/verify' + nik;
     api.put(fullPath, req.body).then(resp => {
         res.send(resp.data);
+    }).catch(error => {
+        res.status(error.response.status);
+        res.send(error);
     });
 });
 
