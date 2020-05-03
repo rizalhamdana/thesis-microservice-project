91b8b108e2cb55d3da8a982f26ba8c739836c097
rizalhamdana <ari.gusti12@gmail.com>
2020-05-03 20:43:38 +0700

    add verify birth regis routing rules in api-gateway

--- api-gateway/routers/birth.js
+++ api-gateway/routers/birth.js
@@ -1,61 +1,90 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var adapter = require('./adapter');
-var isAuth = require('../middlewares/authentication');
+var adapter = require("./adapter");
+var isAuth = require("../middlewares/authentication");
 
-var BASE_URL = 'http://birth-service:8081';
+var BASE_URL = "http://birth-service:8081";
 
 var api = adapter(BASE_URL);
-router.get('/birth', isAuth, (req, res) => {
-    var fullPath = '/api/v1/birth';
-    api.get(fullPath).then(resp => {
-        res.send(resp.data);
-    }).catch(error => {
-        res.status(error.response.status);
-        res.send(error);
+router.get("/birth", isAuth, (req, res) => {
+  var fullPath = "/api/v1/birth";
+  api
+    .get(fullPath)
+    .then((resp) => {
+      res.send(resp.data);
+    })
+    .catch((error) => {
+      res.status(error.response.status);
+      res.send(error);
     });
 });
-router.get('/birth/:birthId', isAuth, (req, res) => {
-    var birthId = req.params['birthId'];
-    var fullPath = '/api/v1/birth/' + birthId;
-    api.get(fullPath).then(resp => {
-        res.send(resp.data);
-    }).catch(error => {
-        res.status(error.response.status);
-        res.send(error);
-    });
-});
-
-router.post('/birth', isAuth, (req, res) => {
-    var fullPath = '/api/v1/birth'
-    api.post(fullPath, req.body).then(resp => {
-        res.send(resp.data);
-    }).catch(error => {
-        res.status(error.response.status);
-        res.send(error);
+router.get("/birth/:birthId", isAuth, (req, res) => {
+  var birthId = req.params["birthId"];
+  var fullPath = "/api/v1/birth/" + birthId;
+  api
+    .get(fullPath)
+    .then((resp) => {
+      res.send(resp.data);
+    })
+    .catch((error) => {
+      res.status(error.response.status);
+      res.send(error);
     });
 });
 
-router.delete('/birth/:birthId', isAuth, (req, res) => {
-    var birthId = req.params['birthId'];
-    var fullPath = '/api/v1/birth/' + birthId;
-    api.delete(fullPath).then(resp => {
-        res.send(resp.data);
-    }).catch(error => {
-        res.status(error.response.status);
-        res.send(error);
+router.post("/birth", isAuth, (req, res) => {
+  var fullPath = "/api/v1/birth";
+  api
+    .post(fullPath, req.body)
+    .then((resp) => {
+      res.send(resp.data);
+    })
+    .catch((error) => {
+      res.status(error.response.status);
+      res.send(error);
     });
 });
 
-router.put('/birth/:birthId', isAuth, (req, res) => {
-    var birthId = req.params['birthId'];
-    var fullPath = '/api/v1/birth/' + birthId;
-    api.put(fullPath, req.body).then(resp => {
-        res.send(resp.data);
-    }).catch(error => {
-        res.status(error.response.status);
-        res.send(error);
+router.delete("/birth/:birthId", isAuth, (req, res) => {
+  var birthId = req.params["birthId"];
+  var fullPath = "/api/v1/birth/" + birthId;
+  api
+    .delete(fullPath)
+    .then((resp) => {
+      res.send(resp.data);
+    })
+    .catch((error) => {
+      res.status(error.response.status);
+      res.send(error);
     });
 });
 
-module.exports = router
+router.put("/birth/:birthId", isAuth, (req, res) => {
+  var birthId = req.params["birthId"];
+  var fullPath = "/api/v1/birth/" + birthId;
+  api
+    .put(fullPath, req.body)
+    .then((resp) => {
+      res.send(resp.data);
+    })
+    .catch((error) => {
+      res.status(error.response.status);
+      res.send(error);
+    });
+});
+
+router.put("/birth/verif/:birthId", isAuth, (req, res) => {
+  var birthId = req.params["birthId"];
+  var fullPath = "/api/v1/birth/verif/" + birthId;
+  api
+    .put(fullPath, req.body)
+    .then((resp) => {
+      res.send(resp.data);
+    })
+    .catch((error) => {
+      res.status(error.response.status);
+      res.send(error);
+    });
+});
+
+module.exports = router;
